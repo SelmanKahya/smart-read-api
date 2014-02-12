bb074ee72c69819c27af8ebecb4e207f67f00b32
Selman K. <selmanhalid@hotmail.com>
2014-02-12 04:37:10 -0800

    switched back to passportjs-local :(
    wasn't able to do auth with passportjs-http digest since browser was
    showing the built-in login pane automatically after getting 401

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var app = express();
 // auth
 // passport library
 var passport = require('passport');
-var passportStrategy = require('passport-http');
-var AUTH = require('./config/auth.js').init(passport, passportStrategy );
-var auth_middleware = passport.authenticate('digest', { session: false });
+var passportStrategy = require('passport-local').Strategy;
+var AUTH = require('./config/auth.js');
+AUTH.init(passport, passportStrategy );
 
 // all environments
 app.use(express.logger('dev'));
@@ -17,9 +17,9 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 
 // auth
-// app.use(express.session({ secret: AUTH.getSecretKey() }));
+app.use(express.session({ secret: AUTH.getSecretKey() }));
 app.use(passport.initialize()); // Add passport initialization
-// app.use(passport.session());    // Add passport initialization
+app.use(passport.session());    // Add passport initialization
 
 app.use(app.router);
 
@@ -34,7 +34,7 @@ if ('development' == app.get('env')) {
 // allow cross site scripting, be careful with that in production
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
@@ -48,18 +48,19 @@ var user = require('./functions/user');
 var member = require('./functions/member');
 var activity = require('./functions/activity');
 var wordLookup = require('./functions/word-lookup');
+var dictionary = require('./functions/dictionary');
 
 // ACTIVITY ROUTES
-app.post(   '/activity', auth_middleware, activity.create);
-app.get(    '/activity/:id', auth_middleware, activity.read);
-app.post(   '/activity/:id', auth_middleware, activity.update);
-app.delete( '/activity/:id', auth_middleware, activity.delete);
+app.post(   '/activity', AUTH.auth, activity.create);
+app.get(    '/activity/:id', AUTH.auth, activity.read);
+app.post(   '/activity/:id', AUTH.auth, activity.update);
+app.delete( '/activity/:id', AUTH.auth, activity.delete);
 
 // WORD LOOKUP
-app.post(   '/word-lookup', auth_middleware, wordLookup.create);
-app.get(    '/word-lookup/:id', auth_middleware, wordLookup.read);
-app.post(   '/word-lookup/:id', auth_middleware, wordLookup.update);
-app.delete( '/word-lookup/:id', auth_middleware, wordLookup.delete);
+app.post(   '/word-lookup', AUTH.auth, wordLookup.create);
+app.get(    '/word-lookup/:id', AUTH.auth, wordLookup.read);
+app.post(   '/word-lookup/:id', AUTH.auth, wordLookup.update);
+app.delete( '/word-lookup/:id', AUTH.auth, wordLookup.delete);
 
 // MEMBERSHIP Routes
 app.post(   '/member/register', member.register);
@@ -68,9 +69,12 @@ app.get(    '/member/loggedin', member.loggedin);
 app.post(   '/member/logout', member.logout);
 
 // USER Routes
-app.get(    '/user/activity', auth_middleware, user.activity);
-app.get(    '/user/word-lookup', auth_middleware, user.lookup);
-app.get(    '/user/word-lookup/books', auth_middleware, user.books);
+app.get(    '/user/activity', AUTH.auth, user.activity);
+app.get(    '/user/word-lookup', AUTH.auth, user.lookup);
+app.get(    '/user/word-lookup/books', AUTH.auth, user.books);
+
+// DICTIONARY
+app.get(    '/dictionary/:word', dictionary.lookup);
 
 // Server is starting, hold on!
 var server = http.createServer(app);
--- config/auth.js
+++ config/auth.js
@@ -1,38 +1,44 @@
 exports.init = function(passport, passportStrategy){
     //==================================================================
     // Define the strategy to be used by PassportJS
-
-    passport.use(new passportStrategy.DigestStrategy(
-        function(username, done) {
+    passport.use(new passportStrategy(
+        function(username, password, done) {
 
             var db = require('../lib/db.js');
             var resultModel = require('../lib/models/result.js');
 
             // user to be authenticated
             var user = {
-                user_email: username
+                user_email: username,
+                user_password: password
             }
 
-            db.execute('SELECT * FROM user WHERE user_email = ?', [user.user_email], function(err, result){
+            db.execute('SELECT * FROM user WHERE user_email = ? AND user_password = ?', [user.user_email, user.user_password], function(err, result){
 
                 if(err)
-                    return done(new resultModel.result(false, {}, ['Error while logging in!']));
+                    return done(null, false, new resultModel.result(false, {}, ['Error while logging in!']));
 
                 else{
                     if(result.length != 1)
-                        return done(null, false);
+                        return done(null, false, new resultModel.result(false, {}, ['Wrong credentials, please try again!']));
 
                     else
-                        done(null, new resultModel.result(true, result[0]), result[0].user_password);
+                        done(null, new resultModel.result(true, result[0]));
                 }
 
             });
-        }     ,
-        function(params, done) {
-            // validate nonces as necessary
-            done(null, true)
         }
     ));
+
+    // Serialized and deserialized methods when got from session
+    passport.serializeUser(function(user, done) {
+        done(null, user);
+    });
+
+    passport.deserializeUser(function(user, done) {
+        done(null, user);
+    });
+
 }
 
 
