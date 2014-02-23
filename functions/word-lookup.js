281e3875877182adedf0420c65d7a431798069e4
Selman K. <selmanhalid@hotmail.com>
2014-02-22 21:14:15 -0800

    updating delete word lookup endpoint
    instead of deleting, set is_deleted column to 1.

--- functions/word-lookup.js
+++ functions/word-lookup.js
@@ -4,7 +4,7 @@ var resultModel = require('../lib/models/result.js');
 // return word-lookup by id
 exports.read = function(req, res){
     var word_lookup_id = req.params.id;
-    db.execute('SELECT * FROM word_lookup WHERE word_lookup_id = ?', [word_lookup_id], function(err, result){
+    db.execute('SELECT * FROM word_lookup WHERE word_lookup_id = ? AND word_lookup_is_deleted != 1', [word_lookup_id], function(err, result){
         if(err)
             res.send(500, new resultModel.result(false, {}, 'Error while getting word lookup data!'));
 
@@ -69,13 +69,42 @@ exports.delete = function(req, res){
 
     var word_lookup_id = req.params.id;
 
-    db.execute('DELETE FROM word_lookup WHERE word_lookup_id = ?', [word_lookup_id], function(err, result){
+    db.execute('SELECT * FROM word_lookup WHERE word_lookup_id = ?', [word_lookup_id], function(err, result){
         if(err)
-            res.send(500, new resultModel.result(false, {}, 'Error while deleting the word-lookup!'));
+            res.send(500, new resultModel.result(false, {}, 'Error while getting word lookup data!'));
 
-        else
-            res.send(new resultModel.result(true, {}));
+        else {
 
-        res.end();
+            if(result.length != 1){
+                res.send(500, new resultModel.result(false, {}, 'Error while getting word lookup data!'));
+
+            }
+
+            // user can only delete his/her own data
+            else if(result[0].user_id != req.user.result.user_id){
+                res.send(401, new resultModel.result(false, {}, 'Permission denied!'));
+
+            }
+
+            else {
+
+                var word = {
+                    user_id: result[0].user_id,
+                    word_lookup_word: result[0].word_lookup_word
+                }
+
+                // never delete, just set is_deleted to 1
+                db.execute('UPDATE word_lookup SET word_lookup_is_deleted = 1 WHERE user_id = ? AND word_lookup_word = ?', [word.user_id, word.word_lookup_word], function(err, result){
+
+                    if(err)
+                        res.send(500, new resultModel.result(false, {}, 'Error while deleting the word-lookup!'));
+
+                    else
+                        res.send(new resultModel.result(true, {}));
+
+                });
+            }
+        }
     });
+
 };
