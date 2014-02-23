52ff162f7741d39ef1fa2a2f254d0a73e5ff656e
Selman K. <selmanhalid@hotmail.com>
2014-02-22 21:13:40 -0800

    adding word_lookup_is_deleted control

--- functions/user.js
+++ functions/user.js
@@ -19,7 +19,7 @@ exports.lookup = function(req, res){
     var user_id = req.user.result.user_id;
     db.execute('SELECT word_lookup_id, word_lookup_word, word_lookup_quiz_taken, word_lookup_quiz_result, ' +
         'word_lookup_created_time, book_name, user_id, COUNT(word_lookup_id) AS word_lookup_count ' +
-        'FROM word_lookup WHERE user_id = ? GROUP BY word_lookup_word', [user_id], function(err, result){
+        'FROM word_lookup WHERE user_id = ?  AND word_lookup_is_deleted != 1 GROUP BY word_lookup_word', [user_id], function(err, result){
         if(err)
             res.send(500, new resultModel.result(false, {}, ['Error while getting word lookup data!']));
 
