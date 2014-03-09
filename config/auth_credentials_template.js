3abcc08e870937c2e2674efb9a028994da3d8c16
Selman K. <selmanhalid@gmail.com>
2014-03-09 15:50:55 -0700

    started using wordnik api

--- config/auth_credentials_template.js
+++ config/auth_credentials_template.js
@@ -1,4 +1,5 @@
 // Rename this file to auth_credentials.js
 exports.credentials = {
-    secret_key: "Your.secret.key.goes.here"
+    secret_key: "Your.secret.key.goes.here",
+    wordnik_secret: "Wordnik api key goes here"
 }
