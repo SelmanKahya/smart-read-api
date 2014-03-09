16bed0ecfb08eb06506baff6ab2b2cdf607fb355
Selman K. <selmanhalid@gmail.com>
2014-03-09 15:51:41 -0700

    refactor dictionary

--- functions/dictionary.js
+++ functions/dictionary.js
@@ -1,20 +1,14 @@
-var request = require('request');
 var resultModel = require('../lib/models/result.js');
+var wordnik = require('../lib/wordnik.js');
 
-// return word defitinion
+// return word definition
 exports.lookup = function(req, res){
     var word = req.params.word;
     var limit = 5; // limit the maximum number of results to return
 
-    // Making calls to wordnik dictionary api
-    // See Wordnik API for more information:
-    // http://developer.wordnik.com/
-    var url = 'http://api.wordnik.com:80/v4/word.json/' + word + '/definitions?limit=' + limit + '&includeRelated=false&sourceDictionaries=all&useCanonical=true&includeTags=false&api_key=ef0b53ce4323052b4a0080d4d6d04f8de987b039242b741f8'
-
-    request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.json(new resultModel.result(true, body));
-        }
+    wordnik.getDefinition(word, limit, function(result, definition){
+        if (result)
+            res.json(new resultModel.result(true, definition));
 
         else
             res.json(500, new resultModel.result(false, {}, 'Error while getting word definition!'));
